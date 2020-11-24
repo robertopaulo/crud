4b395ea339acd9effa267ac29ba3b113a70bc4cf
robertopaulo <roberto123chagas@gmail.com>
2020-11-24 13:51:57 -0400

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,13 +1,9 @@
 <!doctype html>
 <html lang="pt-br">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
     <title>Cadastro</title>
   </head>
   <body>
@@ -15,8 +11,6 @@
     <div class="row">
         <div class="col">
         <h1>Cadastro</h1>
-        
-        
         <form action="cadastro_post.php" method="POST">
             <div class="form-group">
                 <label for="nome">Nome</label>
@@ -47,11 +41,8 @@
       </div>
     </div>
   </div>
-    
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-    
   </body>
 </html>
