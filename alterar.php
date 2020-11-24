b582f18ce2a40d85262376d410445858681daf9e
robertopaulo <roberto123chagas@gmail.com>
2020-11-24 13:50:28 -0400

    Update alterar.php

--- alterar.php
+++ alterar.php
@@ -1,11 +1,8 @@
 <!doctype html>
 <html lang="pt-br">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <title>Alteração de Cadastro</title>
@@ -14,8 +11,8 @@
     <?php
         include "conexao.php";
         $id = $_GET['id'];
+    //Retorna uma linha da tabela que possui o ID correspondente, que foi passado pelo método GET
         $sql = "SELECT * FROM pessoa WHERE id = $id";
-
         $dados = mysqli_query($conexao,$sql);
         $linha = mysqli_fetch_assoc($dados);
     ?>
@@ -24,8 +21,7 @@
     <div class="row">
         <div class="col">
         <h1>Cadastro</h1>
-        
-        
+    
         <form action="alterar_post.php" method="POST">
             <div class="form-group">
                 <label for="nome">Nome</label>
@@ -60,7 +56,7 @@
     </div>
   </div>
     
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+   
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
